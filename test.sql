e2622b01f74c2612aac66a6535133999269fd0bc
seosanggyun <seosanggyun@naver.com>
2022-06-02 18:43:46 +0900

    forlee

--- test.sql
+++ test.sql
@@ -18,7 +18,10 @@
 
 -- DROP TABLE movies_movie_genre_18;
 
-UPDATE accounts_user SET casino_points=100000000 WHERE username='김로라';
+-- UPDATE accounts_user SET casino_points=100000000 WHERE username='김로라';
 
 -- SELECT casino_points FROM accounts_user WHERE username='pakdahoon';
 
+-- SELECT name From movies_person WHERE id=73249;
+
+INSERT INTO movies_person values (73249, 'Lee Jung-jae', 2, '/3h5Cfm0X8ohWn7psZkqdNWqXAHH.jpg', 16.054, '1973-03-15', 'Seoul, South Korea');
